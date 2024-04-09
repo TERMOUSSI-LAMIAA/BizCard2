63c6ece589fd1b7ce198b5155b9103acd2ef710b
TERMOUSSI-LAMIAA <lamiaa3105@gmail.com>
2024-04-09 22:29:21 +0000

    vue

---
+++ src/views/visitCard/View.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="card">
+    <h4>visit Cards
+        <router-link to='/visitcard/create' class="btn btn-primary">Add visitCard</router-link>
+    </h4>
+    <p></p>
+</div>
+</template>
